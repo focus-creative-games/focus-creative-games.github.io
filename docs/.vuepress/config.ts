f68402df86f17dd439c894b0894b5e3973545b95
walon <walon@focus-creative-games.com>
2022-09-29 11:41:01 +0800

    [fix] 修复文档错误

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,16 +29,13 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         text: 'HybridCLR文档', 
         link: '/hybridclr/',
         items:[
-          { text: '关于HybridCLR', link: '/hybridclr/index/'},
-          { text: '支持Unity版本', link: '/hybridclr/support_versions/'},
+          { text: '关于HybridCLR', link: '/hybridclr/about/'},
           { text: '快速开始', link: '/hybridclr/start_up/'},
           { text: '性能报告', link: '/hybridclr/benchmark/'},
-          { text: '原理与特性', link: '/hybridclr/performance/limit/'},
           { text: 'FAQ', link: '/hybridclr/faq/'},
          // { text: '常见错误处理', link: '/hybridclr/common_errors/'},
          { text: '常见错误处理', link: '/hybridclr/common_errors/'},
-          { text: '标准问题解决流程', link:'/hybridclr/problem_solving/'},
-          { text:'第三方项目推荐', link:'/hybridclr/recommended-tools/'},
+          { text: 'bug反馈', link:'/hybridclr/bug_reporter/'},
         ]
       },
       {
